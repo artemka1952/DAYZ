c22d67f2bf3d0aab751fe6ee9b3b99c7f485bc78
unknown <bai1541b@k3-426-9.edu.ugrasu>
2016-04-23 13:06:44 +0600

    Заполнение, сортировка

---
+++ DelovayaIgra23042016/src/delovayaigra23042016/DelovayaIgra23042016.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package delovayaigra23042016;
+
+
+class team {//команда
+    String name;
+    int game;
+    int score;
+    
+    team(String s, int i, int sc) {
+        this.name=s;
+        this.game=i;
+        this.score=sc;
+    }
+}
+
+public class DelovayaIgra23042016 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
